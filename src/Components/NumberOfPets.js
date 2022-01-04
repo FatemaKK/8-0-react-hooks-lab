0c1709609cc579040afcdcb2cc94a72f2957a3cc
Fatema Kabir <fatema.kabir@pursuit.org>
2022-01-03 22:45:52 -0500

    footer and navBar functional component + Number Of Pets

--- src/Components/NumberOfPets.js
+++ src/Components/NumberOfPets.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NumberOfPets.css";
 
-class NumberOfPets extends React.Component {
-  render() {
-    return (
-      <section className="num-of-pets">
-        <h4>Number of Pets</h4>
-        <h5>numOfPets</h5>
-        <div>
-          <button id="less">-</button>
-          <button id="more">+</button>
-        </div>
-      </section>
-    );
-  }
-}
+const NumberOfPets = () => {
+  const [numOfPets, setNumOfPets] = useState(0);
+
+  const decrementNum = () => {
+    if (numOfPets > 0) {
+      setNumOfPets((prevNum) => prevNum - 1);
+    }
+  };
+  const incrementNum = () => {
+    setNumOfPets((prevNum) => prevNum + 1);
+  };
+
+  return (
+    <section className="num-of-pets">
+      <h4>Number of Pets</h4>
+      <h5>{numOfPets}</h5>
+      <div>
+        <button onClick={decrementNum} id="less">
+          -
+        </button>
+        <button onClick={incrementNum} id="more">
+          +
+        </button>
+      </div>
+    </section>
+  );
+};
 
 export default NumberOfPets;
